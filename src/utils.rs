740a9ca63893ec00687a9c8d785347c99bb5d1a5
Jacob Pieniazek <jacob@pieniazek.me>
2025-08-06 23:04:35 -0400

    testing husky

--- src/utils.rs
+++ src/utils.rs
@@ -4,5 +4,5 @@ pub fn generate_random_buffer(size: usize) -> Vec<u8> {
     let mut buffer = vec![0u8; size];
     let mut rng = rand::rng();
     rng.fill_bytes(&mut buffer);
-    buffer
+    return buffer;
 }
